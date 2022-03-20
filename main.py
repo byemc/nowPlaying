e701370a0f1603076590e317b172ec32caa1a600
-e <77801870+byemc@users.noreply.github.com>
2022-03-20 11:30:24 +0000

    update

--- main.py
+++ main.py
@@ -1,18 +1,93 @@
-# uses winrt to get the song name and artist name from the Windows APIs
-# and prints them to a file
+import asyncio
+import time
 
-from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionPlaybackInfo as np
-from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionMediaProperties as media
-from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionPlaybackStatus as status
-import winrt
+from winrt.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager
+from winrt.windows.storage.streams import \
+    DataReader, Buffer, InputStreamOptions
 
-def main():
-    # Get all the attributes of `media`
-    print()
+# Code stolen from
+# https://stackoverflow.com/a/66037406
+
+async def get_media_info():
+    sessions = await MediaManager.request_async()
+
+    # This source_app_user_model_id check and if statement is optional
+    # Use it if you want to only get a certain player/program's media
+    # (e.g. only chrome.exe's media not any other program's).
+
+    # To get the ID, use a breakpoint() to run sessions.get_current_session()
+    # while the media you want to get is playing.
+    # Then set TARGET_ID to the string this call returns.
+
+    current_session = sessions.get_current_session()
+    sessionsarray = sessions.get_sessions()
+    if current_session:  # there needs to be a media session running
+        info = await current_session.try_get_media_properties_async()
+
+        # song_attr[0] != '_' ignores system attributes
+        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+
+        # converts winrt vector to list
+        info_dict['genres'] = list(info_dict['genres'])
+
+        return info_dict
+
+    return None
+
+async def read_stream_into_buffer(stream_ref, buffer):
+    readable_stream = await stream_ref.open_read_async()
+    readable_stream.read_async(buffer, buffer.capacity, InputStreamOptions.READ_AHEAD)
+
+def ExportInfo():
+    # Encode info.txt as utf-8
+    with open('title.txt', 'w', encoding='utf-8') as f:
+        f.write(title)
+    with open('artist.txt', 'w', encoding='utf-8') as f:
+        f.write(artist)
 
 
+if __name__ == '__main__':
+    while True:
+        current_media_info = asyncio.run(get_media_info())
+        if current_media_info == None:
+            title = ""
+            artist = ""
+            ExportInfo()
+            continue
+        if current_media_info['playback_type'] == 1: 
+            isPlaying = "Playing"
+        else:
+            isPlaying = "Stopped"
+        
+        #print(current_media_info)
 
-if __name__ == "__main__":
-    main()
-else:
-    print(f"{__name__}: Don't import me! Thanks, Bye.")
+        title = current_media_info['title']
+        artist = current_media_info['artist']
+
+        ExportInfo()
+
+        try:
+            # create the current_media_info dict with the earlier code first
+            thumb_stream_ref = current_media_info['thumbnail']
+
+            # 5MB (5 million byte) buffer - thumbnail unlikely to be larger
+            thumb_read_buffer = Buffer(5000000)
+
+            # copies data from data stream reference into buffer created above
+            asyncio.run(read_stream_into_buffer(thumb_stream_ref, thumb_read_buffer))
+
+            # reads data (as bytes) from buffer
+            buffer_reader = DataReader.from_buffer(thumb_read_buffer)
+            byte_buffer = buffer_reader.read_bytes(thumb_read_buffer.length)
+
+            with open('media_thumb.jpg', 'wb+') as fobj:
+                fobj.write(bytearray(byte_buffer))
+        except AttributeError:
+            with open('media_thumb.jpg', 'wb+') as fobj:
+                with open ('default.jpg', 'rb') as f:
+                    fobj.write(f.read())
+
+        time.sleep(1)
+
+        
